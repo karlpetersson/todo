328c1fafd488ed208c9729296bb0e6fcd6522eff
Karl Petersson <karl.m.petersson@gmail.com>
2016-03-10 13:13:23 +0100

    added helper for concatenating formatted strings

---
+++ src/concat.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "concat.h"
+
+void concat_bind(MutableConcat_t *c, char *buf) {
+	c->cur = buf;
+	c->end = buf + sizeof buf;
+}
+
+void concat_add(MutableConcat_t *c, char const* str, ...) {
+	va_list argp;
+	va_start(argp, str);
+	c->cur += vsprintf(c->cur, str, argp);
+	va_end(argp);
+}
+
+void immConcat_bind(ImmutableConcat_t *c, size_t size) {
+	c->buf = malloc(size * sizeof(char));
+	c->cur = c->buf;
+	c->end = c->buf + size;
+}
+
+void immConcat_add(ImmutableConcat_t *c, char const* str, ...) {
+	va_list argp;
+	va_start(argp, str);
+	c->cur += vsprintf(c->cur, str, argp);
+	va_end(argp);
+}
+
+char* immConcat_get(ImmutableConcat_t *c) {
+	return c->buf;
+}
+
+void immConcat_free(ImmutableConcat_t *c) {
+	free(c->buf);
+}
---
+++ src/concat.h
@@ -0,0 +1,23 @@
+#ifndef CONCAT_H
+#define CONCAT_H
+
+typedef struct {
+	char *cur;
+	char const *end;
+} MutableConcat_t;
+
+typedef struct {
+	char *buf;
+	char *cur;
+	char const *end;
+} ImmutableConcat_t;
+
+void concat_bind(MutableConcat_t *c, char *buf);
+void concat_add(MutableConcat_t *c, char const* str, ...);
+
+void immConcat_bind(ImmutableConcat_t *c, size_t size);
+void immConcat_add(ImmutableConcat_t *c, char const* str, ...);
+char *immConcat_get(ImmutableConcat_t *c);
+void immConcat_free(ImmutableConcat_t *c);
+
+#endif
