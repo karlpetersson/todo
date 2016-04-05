dc47a6a136306243fc1c348e7768eca0a9fd816c
Karl Petersson <karl.m.petersson@gmail.com>
2016-04-05 16:59:40 +0200

    clean up slightly

--- src/todo.c
+++ src/todo.c
@@ -12,41 +12,11 @@
 #define ANSI_COLOR_BRIGHT 	"\x1b[37m"
 #define ANSI_COLOR_RESET 	"\x1b[0m"
 #define ANSI_BOLD 			"\x1b[1m"
-#define TODO_PRIO_CHAR 		'#'
-#define TODO_PRIO_STRING 	"#"
+#define TODO_PRIO_CHAR 		'*'
+#define TODO_PRIO_STRING 	"* "
 #define TODO_ID_STRING 		ANSI_COLOR_BRIGHT "%d  " ANSI_COLOR_NORMAL
 
 static const int MAX_LINE_LENGTH = 256;
-
-static void todo_free(void *data) {
-	free(((Todo_t *)data)->text);
-}
-
-static int todo_compare(void *v1, void *v2) {
-	Todo_t *a = (Todo_t *)v1;
-	Todo_t *b = (Todo_t *)v2;
-	return (a->prio >= b->prio);
-}
-
-static void todo_count(void *data, int index, va_list args) {
-	Todo_t *todo = (Todo_t *)data;
-	int *p = va_arg(args, int*);
-	*p = index;
-}
-
-static void todo_print(void *data, int index, va_list args) {
-	Todo_t *todo = (Todo_t *)data;
-	char buf[MAX_LINE_LENGTH];
-	MutableConcat_t conc;
-
-	concat_bind(&conc, buf);
-	concat_add(&conc, TODO_ID_STRING, index);
-	if(todo->prio) {
-		concat_add(&conc, ANSI_COLOR_RED TODO_PRIO_STRING ANSI_COLOR_RESET);
-	}
-	concat_add(&conc, "%s", todo->text);
-	printf("%s", buf);
-}
 
 static void __render_alt(MutableConcat_t *conc, const char* text, int linenum, int prio, int selected) {
 	concat_add(conc, TODO_ID_STRING, linenum);
@@ -67,19 +37,50 @@ static void __render_alt(MutableConcat_t *conc, const char* text, int linenum, i
 	concat_add(conc, "%s" ANSI_COLOR_RESET, text);
 }
 
-
 static void __render_default(MutableConcat_t *conc, const char* text, int linenum, int prio, int selected) {
-	if(selected == linenum)
+	if(selected == linenum) {
 		concat_add(conc, ANSI_COLOR_BG_GRAY);
+	}
 
 	concat_add(conc, TODO_ID_STRING, linenum);
 
-	if(prio)
+	if(prio) {
 		concat_add(conc, ANSI_COLOR_RED TODO_PRIO_STRING ANSI_COLOR_NORMAL);
+	}
 
 	concat_add(conc, "%s" ANSI_COLOR_RESET, text);
 }
 
+// list free function
+static void todo_free(void *data) {
+	free(((Todo_t *)data)->text);
+}
+
+// list sort compare function
+static int todo_compare(void *v1, void *v2) {
+	Todo_t *a = (Todo_t *)v1;
+	Todo_t *b = (Todo_t *)v2;
+	return (a->prio >= b->prio);
+}
+
+// function for counting todos (when used in list_foreach sets p to last index)
+static void todo_count(void *data, int index, va_list args) {
+	int *p = va_arg(args, int*);
+	*p = index;
+}
+
+// function for printing a todo to stdout
+static void todo_print(void *data, int index, va_list args) {
+	Todo_t *todo = (Todo_t *)data;
+	char buf[MAX_LINE_LENGTH];
+	MutableConcat_t conc;
+
+	concat_bind(&conc, buf);
+	__render_default(&conc, todo->text, index, todo->prio, 0);
+	printf("%s", buf);
+}
+
+// function for rendering todo to a buffer
 static void todo_render(void *data, int index, va_list args) {
 	Todo_t 				*todo 		= (Todo_t *)data;
 	MutableConcat_t 	*conc 		= va_arg(args, MutableConcat_t*);
@@ -102,7 +103,7 @@ static void todo_save(void *data, int index, va_list args) {
 	if(todo->prio) {
 		snprintf(buf, sizeof buf, TODO_PRIO_STRING);
 	}
-	strcat(buf, todo->text);
+	strncat(buf, todo->text, MAX_LINE_LENGTH);
 	fputs(buf, va_arg(args, FILE*));
 }
 
@@ -126,6 +127,7 @@ int todolist_from_file(TodoList_t *tlist, const char *path) {
 		return 0; //TODO: need error codes etc
 	}
 
+	//TODO: error prone, need to validate file
 	while (fgets(line, MAX_LINE_LENGTH, fp) != NULL) {
 		text = line;
 		priority = 0;
@@ -141,16 +143,20 @@ int todolist_from_file(TodoList_t *tlist, const char *path) {
 	return 1;
 }
 
-//TODO: does this alloc twice? once in list_append...
+//TODO: leaks memory mby
 void todolist_add(TodoList_t *tlist, char *text, int prio) {
 	Todo_t todo;
+
 	todo.text = malloc(MAX_LINE_LENGTH * sizeof (char));
 	todo.prio = prio;
+
 	memset(todo.text, 0, MAX_LINE_LENGTH);
-	strcat(todo.text, text);
+	strncat(todo.text, text, MAX_LINE_LENGTH);
+	
 	if(!strchr(todo.text, '\n')) {
-		strcat(todo.text, "\n");
+		strncat(todo.text, "\n", MAX_LINE_LENGTH);
 	}
+
 	list_append(tlist->todos, &todo);
 }
 
@@ -158,6 +164,7 @@ void todolist_finish(TodoList_t *tlist, int *linenum) {
 	list_remove(tlist->todos, *linenum);
 }
 
+// sets priority of todo and sorts the list based on priority
 void todolist_set_priority(TodoList_t *tlist, int *linenum, int *prio) {
 	Todo_t update;
 	Todo_t *todo = (Todo_t*)list_get(tlist->todos, *linenum);
@@ -165,9 +172,10 @@ void todolist_set_priority(TodoList_t *tlist, int *linenum, int *prio) {
 	todo->prio = *prio;
 
 	list_set(tlist->todos, *linenum, todo);
-	list_sort(tlist->todos, todo_compare);	
+	list_sort(tlist->todos, todo_compare);
 }
 
+// prints todos to buffer using specified render function
 void todolist_render(TodoList_t *tlist, char *buf, int selected) {
 	MutableConcat_t concat;
 	concat_bind(&concat, buf);
@@ -179,6 +187,7 @@ void todolist_print(TodoList_t *tlist) {
 	printf("-----\n");
 }
 
+// prints each todo in todolist to specified file
 int todolist_save(TodoList_t *tlist, const char *path) {
 	FILE *fp;
 	if(!(fp = fopen(path, "w"))) {
@@ -191,9 +200,5 @@ int todolist_save(TodoList_t *tlist, const char *path) {
 }
 
 void todolist_destroy(TodoList_t *tlist) {
-	//TODO: call list_free etc
-}
-
-void todolist_load(TodoList_t *tlist, char *files) {
-	//TODO: load from source files EPIC
+	list_free(tlist->todos);
 }
