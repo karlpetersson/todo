df66fa21f4964f6ba0fc157d28cfe5856c38a9d3
Karl Petersson <karl.m.petersson@gmail.com>
2016-04-15 17:52:18 +0200

    lots of cleanup, added error codes

--- src/styles.c
+++ src/styles.c
@@ -1,65 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "styles.h"
 #include "cJSON.h"
+#include "error.h"
+#include "cbuf.h"
+#include "colors.h"
 
-#define BG_GRAY 	"\x1B[40m"
-#define BG_RED  	"\x1B[41m"
-#define BG_GREEN  	"\x1B[42m"
-#define BG_YELLOW  	"\x1B[44m"
-#define BG_BLUE  	"\x1B[44m"
-#define BG_MAGENTA  "\x1B[45m"
-#define BG_CYAN  	"\x1B[46m"
-#define BG_WHITE  	"\x1B[47m"
-
-#define CO_BLACK  	"\x1B[30"
-#define CO_RED  	"\x1B[31m"
-#define CO_GREEN  	"\x1B[32m"
-#define CO_YELLOW  	"\x1B[33m"
-#define CO_BLUE  	"\x1B[34m"
-#define CO_MAGENTA  "\x1B[35m"
-#define CO_CYAN  	"\x1B[36m"
-#define CO_WHITE  	"\x1B[37m"
-
-#define ST_NORMAL 		"\x1b[39m"
-#define ST_RESET 		"\x1b[0m"
-#define ST_BOLD 		"\x1b[1m"
-#define ST_FAINT 		"\x1b[2m"
-#define ST_ITALIC 		"\x1b[3m"
-#define ST_UNDERLINE 	"\x1b[4m"
-#define ST_BLINK 		"\x1b[5m"
-#define ST_FBLINK 		"\x1b[6m"
-#define ST_IMAGE 		"\x1b[7m"
-#define ST_CONCEAL 		"\x1b[8m"
-#define ST_CROSS 		"\x1b[9m"
-
-static const char *co_table[8] = {
-	/*0 = BLACK*/  	"\x1B[30",
-	/*1 = RED*/  	"\x1B[31m",
-	/*2 = GREEN*/  	"\x1B[32m",
-	/*3 = YELLOW*/  "\x1B[33m",
-	/*4 = BLUE*/  	"\x1B[34m",
-	/*5 = MAGENTA*/ "\x1B[35m",
-	/*6 = CYAN*/  	"\x1B[36m",
-	/*7 = WHITE*/  	"\x1B[37m"
-};
-
-static const char *bg_table[8] = {
-	/*0 = GRAY*/ 	"\x1B[40m",
-	/*1 = RED*/  	"\x1B[41m",
-	/*2 = GREEN*/  	"\x1B[42m",
-	/*3 = YELLOW*/  "\x1B[44m",
-	/*4 = BLUE*/  	"\x1B[44m",
-	/*5 = MAGENTA*/ "\x1B[45m",
-	/*6 = CYAN*/  	"\x1B[46m",
-	/*7 = WHITE*/  	"\x1B[47m"
-};
-
-static const char *st_table[3] = {
-	/*0 = BOLD*/ 		"\x1b[1m",
-	/*1 = UNDERLINE*/ 	"\x1b[4m",
-	/*2 = IMAGE*/ 		"\x1b[7m"
-};
+#define ST_RESET "\x1b[0m"
 
 static char *__json_str_from_file(const char *filename) {
 	char *json_str;
@@ -82,20 +30,14 @@ static char *__json_str_from_file(const char *filename) {
 	return json_str;
 }
 
-static int __style_parser_error(const char *msg) {
-	fprintf(stderr, "Style parser error: %s\n", msg);
-	return 0;
-}
-
-static int __parse_style(char *style_raw, char *result) {
+static int __parse_style(char *style_raw, cbuf_t *result) {
 	char *ptr = NULL;
 	char cToStr[2];
+	const char *mod;
 	int found[3] = {0,0,0};
-	int num, fcnt = 0;
+	int fcnt = 0;
 
 	cToStr[1] = '\0';
-
-	strcat(style_raw, "$(R)");
 
 	ptr = style_raw;
 
@@ -104,34 +46,35 @@ static int __parse_style(char *style_raw, char *result) {
 			ptr += 2; //skip '$' and '('
 
 			if(*ptr == 'c' || *ptr == 'C') {
-				num = *(++ptr) - '0';
-				if(num < 0 || num > 8)
-					return __style_parser_error("Num for text color out of bounds (0-7 allowed)");
-				strcat(result, co_table[num]);
+				if(!(mod = co_from_id(*(++ptr) - '0'))) return TODO_ESTL_CRNUM;
+				cbuf_puts(result, mod, strlen(mod));
+
 			} else if(*ptr == 'b' || *ptr == 'B') {
-				num = *(++ptr) - '0';
-				if(num < 0 || num > 8)
-					return __style_parser_error("Num for background color out of bounds (0-7 allowed)");
-				strcat(result, bg_table[num]);
+				if(!(mod = bg_from_id(*(++ptr) - '0'))) return TODO_ESTL_BGNUM;
+				cbuf_puts(result, mod, strlen(mod));
+
 			} else if(*ptr == 's' || *ptr == 'S') {
-				num = *(++ptr) - '0';
-				if(num < 0 || num > 8)
-					return __style_parser_error("Num for special out of bounds (0-2 allowed)");
-				strcat(result, st_table[num]);
+				if(!(mod = st_from_id(*(++ptr) - '0'))) return TODO_ESTL_SPNUM;
+				cbuf_puts(result, mod, strlen(mod));
+
 			} else if(*ptr == 'r' || *ptr == 'R') {
-				strcat(result, ST_RESET);
+				cbuf_puts(result, ST_RESET, strlen(ST_RESET));
+
 			} else if(*ptr == 'n' || *ptr == 'N') {
-				if(fcnt > 2) return __style_parser_error("Style contains more than one of each $(N) $(P) and $(T)");
+				if(fcnt > 2) return TODO_ESTL_WFRMT;
 				found[fcnt++] = 'N';
-				strcat(result, "%d");
+				cbuf_puts(result, "%d", 2);
+
 			} else if(*ptr == 'p' || *ptr == 'P') {
-				if(fcnt > 2) return __style_parser_error("Style contains more than one of each $(N) $(P) and $(T)");
+				if(fcnt > 2) return TODO_ESTL_WFRMT;
 				found[fcnt++] = 'P';
-				strcat(result, "%s");
+				cbuf_puts(result, "%s", 2);
+
 			} else if(*ptr == 't' || *ptr == 'T') {
-				if(fcnt > 2) return __style_parser_error("Style contains more than one of each $(N) $(P) and $(T)");
+				if(fcnt > 2) return TODO_ESTL_WFRMT;
 				found[fcnt++] = 'T';
-				strcat(result, "%s");
+				cbuf_puts(result, "%s", 2);
+
 			} else {
 				printf("Unknown variable: %c \n", *ptr);
 			}
@@ -139,78 +82,81 @@ static int __parse_style(char *style_raw, char *result) {
 			ptr += 2; // skip var and ')'
 		} else {
 			cToStr[0] = *ptr++;
-			strcat(result, cToStr);
+			cbuf_puts(result, cToStr, strlen(cToStr));
 		}
 	}
 
+	// reset styles at the end
+	cbuf_puts(result, ST_RESET, strlen(ST_RESET));
+
 	if(fcnt < 3)
-		return __style_parser_error("One of the variables $(N) $(P) $(T) is missing");
+		return TODO_ESTL_VMSNG;
 
 	if(!(found[0] == 'N' && found[1] == 'P' && found[2] == 'T'))
-		return __style_parser_error("Variables for line number, priority and text is in wrong order (correct order is: $(N) $(P) $(T))");
+		return TODO_ESTL_WORDR;
 
-	return 1;
+	return TODO_OK;
 
 }
 
-void apply_style(cbuf_t *result, const char *text, int linenum, int prio, int selected, Style_t *style) {
+void styles_apply(cbuf_t *result, const char *text, int linenum, int prio, int selected, Style_t *style) {
 	char line[STYLE_LINE_LENGTH];
 
-	sprintf(line, ((selected == linenum) ? style->selected : style->line),
-		linenum, (prio ? style->prioString : ""), text); 
+	sprintf(line, ((selected == linenum) ? cbuf_get(style->text_sel) : cbuf_get(style->text_norm)),
+		linenum, (prio ? cbuf_get(style->prio_str) : ""), text);
 	
 	cbuf_puts(result, line, strlen(line));
+}
 
-	//strncat(result, line, STYLE_LINE_LENGTH);
-	//strncat(result, ST_RESET, 8);
+Style_t *styles_init() {
+	Style_t *style = malloc(sizeof(Style_t));
+	style->text_norm = cbuf_new(STYLE_LINE_LENGTH);
+	style->text_sel = cbuf_new(STYLE_LINE_LENGTH);
+	style->prio_str = cbuf_new(STYLE_LINE_LENGTH);
+	return style;
+}
 
-	//printf("LEN: %lu  ", strlen(line));
-
-	//concat_add(conc, STYLE_LINE_LENGTH, "%s" ST_RESET, line);
+void styles_free(Style_t *style) {
+	cbuf_free(style->text_norm);
+	cbuf_free(style->text_sel);
+	free(style);
 }
 
 int styles_from_json(Style_t *style, const char *path) {
 	const char *active_style_id;
 	char *json_str = __json_str_from_file(path);
+	char *prioString;
+	cJSON *root, *styles, *active_style;
+	todo_error_t res;
 
 	if(!json_str) {
-		//TODO: error handling Plz
-		fprintf(stderr, "Cannot open file %s\n", path);
-		return 0;
+		return TODO_ESTL_NOFILE;
 	}
-
-	char style_str[STYLE_LINE_LENGTH] = {0};
-	char style_str_sel[STYLE_LINE_LENGTH] = {0};	
-	cJSON *root, *styles, *active_style;
 
 	root = cJSON_Parse(json_str);
 	active_style_id = cJSON_GetObjectItem(root, "activeStyle")->valuestring;
 	styles = cJSON_GetObjectItem(root, "styles");
 	
 	if((active_style = cJSON_GetObjectItem(styles, active_style_id)) == NULL) {
-		//TODO: error handling pLZ
-		fprintf(stderr, "The active style is set to '%s', but it is not defined in the styles-file\n", active_style_id);
-		return 0;
+		print_user_error(TODO_ESTL_NOACTV, active_style_id);
+		return TODO_ESTYLE;
 	} 
 
-	style->line[0] = '\0';
-	style->selected[0] = '\0';
-	style->prioString[0] = '\0';
+	prioString = cJSON_GetObjectItem(active_style, "prioString")->valuestring;
+	cbuf_puts(style->prio_str, prioString, strlen(prioString));
 
-	strncat(style_str, cJSON_GetObjectItem(active_style, "line")->valuestring, 1024);
-	strncat(style_str_sel, cJSON_GetObjectItem(active_style, "selected")->valuestring, 1024);
-	strncat(style->prioString, cJSON_GetObjectItem(active_style, "prioString")->valuestring, 64);
-
-	if(!__parse_style(style_str, style->line)) {
-		fprintf(stderr, "Error in style '%s' attribute 'line'\n", active_style_id);
-		return 0;
+	if((res = __parse_style(cJSON_GetObjectItem(active_style, "line")->valuestring, 
+			style->text_norm)) != TODO_OK) {
+		print_user_error(res, active_style_id);
+		return TODO_ESTYLE;
 	}
-	if(!__parse_style(style_str_sel, style->selected)) {
-		fprintf(stderr, "Error in style '%s' attribute 'selected'\n", active_style_id);
-		return 0;
+	if((res = __parse_style(cJSON_GetObjectItem(active_style, "selected")->valuestring, 
+			style->text_sel)) != TODO_OK) {
+		print_user_error(res, active_style_id);
+		return TODO_ESTYLE;
 	}
 
 	cJSON_Delete(root);
 
-	return 1;
+	return TODO_OK;
 }
--- src/styles.h
+++ src/styles.h
@@ -1,18 +1,20 @@
 #ifndef STYLES_H
 #define STYLES_H
 
-#include "concat.h"
 #include "cbuf.h"
 
 #define STYLE_LINE_LENGTH (2048)
 
 typedef struct Style_t {
-	char line[STYLE_LINE_LENGTH];
-	char selected[STYLE_LINE_LENGTH];
-	char prioString[65];
+	cbuf_t *text_norm;
+	cbuf_t *text_sel;
+	cbuf_t *prio_str;
 } Style_t;
 
-void apply_style(cbuf_t *result, const char *text, int linenum, int prio, int selected, Style_t *style);
+
+Style_t *styles_init();
+void styles_free(Style_t *style);
+void styles_apply(cbuf_t *result, const char *text, int linenum, int prio, int selected, Style_t *style);
 int styles_from_json(Style_t *style, const char *path);
 
 #endif
