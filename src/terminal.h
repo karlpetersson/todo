836d9425bac7d412c41eb3a828289fdeea8b3461
Karl Petersson <karl.m.petersson@gmail.com>
2016-04-15 17:52:59 +0200

    removed unused define

--- src/terminal.h
+++ src/terminal.h
@@ -1,8 +1,6 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
-#define TERM_WRITE_MAX_SIZE (4096)
-
 void term_render(char *buf, size_t size);
 void term_get_key_input(char *result, size_t size);
 void term_get_text_input(char *result, size_t size);
